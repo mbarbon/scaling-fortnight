dfd36ce6f68e9ae0e09031370a349e74ed61f393
Mattia Barbon <mattia@barbon.org>
2017-12-17 11:52:16 +0100

    More testing

---
+++ go/scaling-fortnight/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mbarbon/scaling-fortnight/go/util"
+
+func main() {
+	util.Print("Hello, world")
+}
---
+++ go/util/util.go
@@ -0,0 +1,7 @@
+package util
+
+import "fmt"
+
+func Print(s string) {
+	fmt.Println(s)
+}
